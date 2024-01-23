8044080bb9caeac236bc0f25d68c40c1d54c9fa1
AlizadaQanira04 <gd7mhxn6a@code.edu.az>
2024-01-23 19:41:59 +0400

    Florist

--- Florist-website/assets/js/admin.js
+++ Florist-website/assets/js/admin.js
@@ -1,9 +1,10 @@
 let BASE_URL = `http://localhost:8080`;
 let tBody = document.querySelector("tbody");
-let search = document.querySelector(".search");
+let numberAsc = document.querySelector(".number-asc");
 let ascBtn = document.querySelector(".asc-button");
 let form = document.querySelector("form");
-let allInputs=document.querySelectorAll("input")
+let allInputs = document.querySelectorAll("input");
+let editId = null;
 let arr = [];
 async function getData() {
   let response = await axios(`${BASE_URL}/products`);
@@ -12,7 +13,7 @@ async function getData() {
   drawTable(response.data);
 }
 getData();
-
+//DRAWTABLE
 function drawTable(data) {
   tBody.innerHTML = "";
   data.forEach((element) => {
@@ -23,26 +24,82 @@ function drawTable(data) {
     <td>${element.title}</td>
     <td>${element.price}</td>
     <td>${element.category}</td>
-    <td><button class="delete-button">DELETE</button></td>
-    <td><button class="edit-button">EDIT</button></td>
+    <td><button class="delete-button" onclick=deleteProduct("${element.id}",this)>DELETE</button></td>
+    <td><button class="edit-button" onclick=editProduct("${element.id}")>EDIT</button></td>
     `;
     tBody.append(trElement);
   });
 }
+//DELETE
+async function deleteProduct(id, btn) {
+  if (confirm("do you want delete this product??")) {
+    await axios.delete(`${BASE_URL}/products/${id}`);
+    btn.closest.remove();
+  }
+}
+//ACS
+ascBtn.addEventListener("click", function () {
+  if (ascBtn.innerText === "A-Z") {
+    ascBtn.innerText = "Z-A";
+    let sorted = arr.sort((a, b) => (a.title > b.title ? 1 : -1));
+    drawTable(sorted);
+  } else {
+    ascBtn.innerText = "A-Z";
+    let sorted = arr.sort((a, b) => (a.title < b.title ? 1 : -1));
+    drawTable(sorted);
+  }
+});
+numberAsc.addEventListener("click", function () {
+  if (ascBtn.innerText === "ASC") {
+    ascBtn.innerText = "DSC";
+    let sorted = arr.sort((a, b) => a.price - b.price);
+    drawTable(sorted);
+  } else {
+    ascBtn.innerText = "ASC";
+    let sorted = arr.sort((a, b) => b.price - a.price);
+    drawTable(sorted);
+  }
+});
+
+//FORM
+form.addEventListener("submit", async function (event) {
+  event.preventDefault();
+  const obj = {
+    imageUrl: allInputs[0].value,
+    title: allInputs[1].value,
+    category: allInputs[2].value,
+    price: allInputs[3].value,
+  };
+  if (
+    allInputs[0].value &&
+    allInputs[1].value &&
+    allInputs[2].value &&
+    allInputs[3].value
+  ) {
+    if (!editId) {
+      await axios.post(`${BASE_URL}/products`, obj);
+    } else {
+      await axios.patch(`${BASE_URL}/products/${editId}`, obj);
+    }
+  } else {
+    window.alert("what do you  do!!!?");
+  }
+  allInputs.forEach((item) => (item.value = ""));
+});
+async function editProduct(id) {
+  editId = id;
+  window.scrollTo(0, 0);
+  let response = await axios(`${BASE_URL}/products/${id}`);
+  (allInputs[0].value = response.data.imageUrl),
+    (allInputs[1].value = response.data.title),
+    (allInputs[2].value = response.data.category),
+    (allInputs[3].value = response.data.price);
+}
+//SEARCH
+let search = document.querySelector(".search-inp");
 search.addEventListener("input", function (e) {
   let filtered = arr.filter((item) =>
     item.title.toLocaleLowerCase().includes(e.target.value.toLocaleLowerCase())
   );
   drawTable(filtered);
-});
-ascBtn.addEventListener("click", function () {
-  if (ascBtn.innerText === "ASC") {
-    ascBtn.innerText = "DSC";
-    let sorted = arr.sort((a, b) => (a.title > b.title ? 1 : -1));
-    drawTable(sorted);
-  } else {
-    ascBtn.innerText = "ASC";
-    let sorted = arr.sort((a, b) => (a.title < b.title ? 1 : -1));
-    drawTable(sorted);
-  }
 });
--- Florist-website/assets/js/main.js
+++ Florist-website/assets/js/main.js
@@ -2,11 +2,11 @@ let BASE_URL = `http://localhost:8080`;
 let products = document.querySelector(".products");
 let arr = [];
 let allBtn = document.querySelector(".btn-all");
-let btn2 = document.querySelector(".btn1");
-let btn3 = document.querySelector(".btn2");
-let btn4 = document.querySelector(".btn3");
-let btn5 = document.querySelector(".btn4");
-let btn6 = document.querySelector(".btn5");
+let btn2 = document.querySelector(".btn2");
+let btn3 = document.querySelector(".btn3");
+let btn4 = document.querySelector(".btn4");
+let btn5 = document.querySelector(".btn5");
+let btn6 = document.querySelector(".btn6");
 async function getData() {
   let response = await axios(`${BASE_URL}/products`);
   console.log(response.data);
@@ -14,16 +14,47 @@ async function getData() {
   drawProduct(response.data);
 }
 getData();
-// allBtn.addEventListener("click", function () {
-//   drawProduct(arr);
-// });
 
-// btn2.addEventListener("click", function () {
-//   let filtered = arr.filter(
-//     (item) => item.category.toLocaleLowerCase() === "Bouquet"
-//   );
-//   drawProduct(filtered);
-// });
+allBtn.addEventListener("click", function () {
+  // console.log(allBtn);
+  drawProduct(arr);
+});
+
+btn2.addEventListener("click", function () {
+  // console.log(btn2);
+  let filtered = arr.filter(
+    (item) => item.category.toLocaleLowerCase() === "bouquet"
+  );
+  drawProduct(filtered);
+});
+btn3.addEventListener("click", function () {
+  // console.log(btn2);
+  let filtered = arr.filter(
+    (item) => item.category.toLocaleLowerCase() === "flower box"
+  );
+  drawProduct(filtered);
+});
+btn4.addEventListener("click", function () {
+  // console.log(btn2);
+  let filtered = arr.filter(
+    (item) => item.category.toLocaleLowerCase() === "flower shelf"
+  );
+  drawProduct(filtered);
+});
+btn5.addEventListener("click", function () {
+  // console.log(btn2);
+  let filtered = arr.filter(
+    (item) => item.category.toLocaleLowerCase() === "basket of flower"
+  );
+  drawProduct(filtered);
+});
+btn6.addEventListener("click", function () {
+  // console.log(btn2);
+  let filtered = arr.filter(
+    (item) => item.category.toLocaleLowerCase() === "gift combos"
+  );
+  drawProduct(filtered);
+});
 
 function drawProduct(data) {
   products.innerHTML = "";
@@ -31,7 +62,7 @@ function drawProduct(data) {
     products.innerHTML += `
     <div class="product">
     <img src="${element.imageUrl}" alt="" />
-    <h3>${element.title}</h3>
+    <h4>${element.title}</h4>
     <p>$ ${element.price}</p>
     <a href="./details.html?id=${element.id}">LEARN MORE</a>
   </div>
